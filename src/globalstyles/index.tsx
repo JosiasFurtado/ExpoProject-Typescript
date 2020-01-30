0c1cc19f2a84cfeccbdc023636824aad035290e7
Josias Furtado <jo.junior94@gmail.com>
2020-01-30 16:13:21 -0300

    global styles and test modal view in main page

--- src/globalstyles/index.tsx
+++ src/globalstyles/index.tsx
@@ -1,8 +1,23 @@
 import { StyleSheet } from "react-native";
 
 const s = StyleSheet.create({
+  backgroundModal: {
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    flex: 1
+  },
   backgroundPrimary: {
-    backgroundColor: "#000"
+    backgroundColor: "#2F80ED"
+  },
+  backgroundSecondary: {
+    backgroundColor: "#56CCF2"
+  },
+  borderRadius: {
+    borderRadius: 8
+  },
+  buttonDefault: {
+    borderRadius: 8,
+    height: 45,
+    width: "80%"
   },
   center: {
     alignItems: "center",
@@ -13,6 +28,10 @@ const s = StyleSheet.create({
   },
   flex1: {
     flex: 1
+  },
+  marginModal: {
+    marginHorizontal: 16,
+    marginVertical: 16
   },
   txtBold: {
     fontWeight: "bold"
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,12 +1,60 @@
-import React from "react";
-import { SafeAreaView, View, Text } from "react-native";
+import React, { useState } from "react";
+import {
+  SafeAreaView,
+  View,
+  Text,
+  Modal,
+  TouchableOpacity
+} from "react-native";
 import s from "../../globalstyles";
 
-const Main: React.FC = () => {
+interface TestLint {
+  undefined;
+}
+
+type Props = {
+  undefined;
+};
+
+const Main: React.FC<Props> = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+
   return (
     <SafeAreaView style={[s.backgroundPrimary, s.flex1]}>
       <View style={[s.center, s.flex1]}>
         <Text style={[s.colorSecondary, s.center, s.txtBold]}>Main Page</Text>
+        <TouchableOpacity
+          onPress={() => setModalVisible(true)}
+          style={[s.center, s.buttonDefault, s.backgroundSecondary]}
+        >
+          <Text style={[s.colorSecondary, s.txtBold]}>Open Modal</Text>
+        </TouchableOpacity>
+        <Modal
+          animationType="slide"
+          transparent
+          visible={modalVisible}
+          onRequestClose={() => setModalVisible(false)}
+        >
+          <View style={s.backgroundModal}>
+            <View
+              style={[
+                s.backgroundSecondary,
+                s.marginModal,
+                s.flex1,
+                s.center,
+                s.borderRadius
+              ]}
+            >
+              <Text style={[s.colorSecondary, s.txtBold]}>Inside Modal</Text>
+              <TouchableOpacity
+                onPress={() => setModalVisible(false)}
+                style={[s.center, s.buttonDefault, s.backgroundPrimary]}
+              >
+                <Text style={[s.colorSecondary, s.txtBold]}>Close Modal</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Modal>
       </View>
     </SafeAreaView>
   );
